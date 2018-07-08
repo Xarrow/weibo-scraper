f60484c149be7dac3fe0eb3d80bd73f996687a44
Xarrow <zhangjian.dev@gmail.com>
2018-07-08 23:56:06 +0800

    Python Dependency Inject

--- samples/tweets_persistence/tweets_persistence.py
+++ samples/tweets_persistence/tweets_persistence.py
@@ -21,17 +21,17 @@ datefmt = '%Y-%m-%d %H:%M'
 logging.basicConfig(level=level, format=format, datefmt=datefmt)
 logger = logging.getLogger(__name__)
 
-
 DEFAULT_EXPORT_FILENAME = "export_%s" % int(time.time())
 DEFAULT_EXPORT_PATH = os.getcwd()
 
+
 class WeiboScraperPersistenceException(Exception):
     def __init__(self, message):
-        Exception.__init__(message)
+        self.message = message
 
 
 @contextmanager
-def open_file(file_name: str = r'template/weibo_tweets.html'):
+def open_file(file_name: str):
     file = open(file=file_name, mode='wb')
     yield file
     file.flush()
@@ -39,7 +39,13 @@ def open_file(file_name: str = r'template/weibo_tweets.html'):
 
 
 class BaseAction(object):
-    def execute(self,*args,**kwargs):
+    def __init__(self,name:str,pages:int=None):
+        if name is None or name == '':
+            raise WeiboScraperPersistenceException("JSON persistence need param of 'name' which you want to search!")
+        self.name = name
+        self.pages = pages
+
+    def execute(self, *args, **kwargs):
         pass
 
 
@@ -47,8 +53,9 @@ class TweetsPersistence(object):
     def __init__(self, action: BaseAction):
         self.action = action
 
-    def persistence(self,*args,**kwargs):
-        self.action.execute(*args,**kwargs)
+    def persistence(self, *args, **kwargs):
+        # TODO function to AOP
+        self.action.execute(*args, **kwargs)
 
 
 class FilePersistence(BaseAction):
@@ -76,41 +83,30 @@ class SQLPersistence(BaseAction):
     pass
 
 
-import json
-
 class JSONPersistence(BaseAction):
     """ export as JSON"""
 
     def __init__(self,
                  export_file_path: str = DEFAULT_EXPORT_PATH,
-                 export_file_name: str = DEFAULT_EXPORT_FILENAME+ '.json',
+                 export_file_name: str = DEFAULT_EXPORT_FILENAME + '.json',
                  name: str = None,
                  pages: int = None, ) -> None:
-        if name is None or name=='':
-            raise WeiboScraperPersistenceException("JSON persistence need param of 'name' which you want to search!")
-        self.name = name
-        self.pages = pages
+        super().__init__(name,pages)
         self.export_file_path = export_file_path
-        self.export_file_name = self.name+"_"+export_file_name
-
+        self.export_file_name = self.name + "_" + export_file_name
 
     def execute(self, *args, **kwargs):
-        if self.name is None:
-            self.name = args or kwargs.get('name')
-        if self.pages is None:
-            self.pages = args or kwargs.get('pages') or None
-        if self.name is None:
-            raise WeiboScraperPersistenceException("JSON persistence need param of 'name' which you want to search!")
         with open_file(file_name=os.path.join(self.export_file_path, self.export_file_name)) as json_file:
             tweets_iterator = get_formatted_weibo_tweets_by_name(name=self.name, pages=self.pages)
             for tweets_parser in tweets_iterator:
                 for tweet_meta in tweets_parser.cards_node:
-                    json_file.write(bytes(str(tweet_meta.raw_card),encoding='utf-8'))
-                    json_file.write(bytes('\t\t\n',encoding='utf-8'))
+                    json_file.write(bytes(str(tweet_meta.raw_card), encoding='utf-8'))
+                    json_file.write(bytes('\t\t\n', encoding='utf-8'))
         pass
 
 
 # filePst = FilePersistence()
-jsonPst = JSONPersistence(name='嘻红豆',pages=None)
+jsonPst = JSONPersistence(name=None, pages=None)
+# jsonPst.execute()
 tPst = TweetsPersistence(action=jsonPst)
-tPst.persistence()
+tPst.persistence(pages=1)
